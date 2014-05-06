a9702a86ba2fd8b8ba362a29f03fa10a3c847115
Patrick Toomey <ptoomey3@biasedcoin.com>
2014-05-06 10:10:21 -0500

    bump to 1.1.2.github

--- lib/aws/s3/version.rb
+++ lib/aws/s3/version.rb
@@ -1,12 +1,12 @@
 module AWS
   module S3
     module VERSION #:nodoc:
-      MAJOR    = '0'
-      MINOR    = '6'
-      TINY     = '3'
-      BETA     = nil # Time.now.to_i.to_s
+      MAJOR    = '1'
+      MINOR    = '1'
+      TINY     = '2'
+      BETA     = 'github'
     end
-    
+
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
   end
 end
